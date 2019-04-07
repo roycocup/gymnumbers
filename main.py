610e3b546d2412108f31bb1f8cb80ea431041cb1
roycocup <rodrigo.pitta@gmail.com>
2019-04-07 15:19:11 +0100

    implementing scheduled

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sched, time
 
-email = 'rodrigo.pitta@gmail.com'
-pin = '93922604'
-url = 'https://www.puregym.com'
+s = sched.scheduler(time.time, time.sleep)
+def run():
+    print("Executing...")
+    email = 'rodrigo.pitta@gmail.com'
+    pin = '93922604'
+    url = 'https://www.puregym.com'
 
 
-b = webdriver.Firefox()
-b.get(url + '/login')
-e = b.find_element(value='email').send_keys(email)
-p = b.find_element(value='pin').send_keys(pin)
-sub = b.find_element(value='login-submit').click()
-b.implicitly_wait(10)
+    b = webdriver.Firefox()
+    b.get(url + '/login')
+    e = b.find_element(value='email').send_keys(email)
+    p = b.find_element(value='pin').send_keys(pin)
+    sub = b.find_element(value='login-submit').click()
+    b.implicitly_wait(10)
 
-mess = b.find_elements_by_class_name('text-center')
-num_people = int(re.search(r'\d+', mess[1].text).group())
-with open('datapoints.txt', 'a') as f:
-    f.write(str(num_people) + "\n")
+    mess = b.find_elements_by_class_name('text-center')
+    num_people = int(re.search(r'\d+', mess[1].text).group())
+    with open('datapoints.txt', 'a') as f:
+        f.write(str(num_people) + "\n")
 
-b.quit()
+    b.quit()
+    print("Done.")
+    s.enter(60 * 5, 1, run)
+
+s.enter(1, 1, run)
+s.run()
+
 
